5185fca82abc4f8552057b8aa2169b9889a0d1a5
Joshua <j.blengino@yahoo.com>
2021-08-13 13:48:18 -0700

    commit tree 4eb45b2dbe750ff7b33da0f132448dba08636a21

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'reliable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'dark brown', 'playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'dark brown', 'thoughtful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'Brown', 'controlled', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'dark brown', 'sociable', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 4, 'M', 'dark brown', 'goofy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'dark brown', 'curious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, 'M', 'black', 'aggressive', 0);
